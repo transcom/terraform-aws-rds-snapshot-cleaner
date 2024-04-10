367c460050508363924e257d81239a1b23792c0c
Edward Harris <edward.harris@caci.com>
2024-04-10 11:22:49 -0400

    B-19459-IAC-UPDATE-2

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ data "aws_iam_policy_document" "main" {
     ]
 
     resources = [
-      # "arn:${data.aws_partition.current.partition}:rds:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:db:${var.cleaner_db_instance_identifier}",
+      "arn:${data.aws_partition.current.partition}:rds:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:db:${var.cleaner_db_instance_identifier}",
       "arn:${data.aws_partition.current.partition}:rds:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:snapshot:${var.cleaner_db_instance_identifier}-*",
     ]
   }
